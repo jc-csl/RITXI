058e3597b8e9e85484387c9ca8ef35a0741115e6
jcsanroman <jcsanroman@centrosanluis.com>
2026-07-07 22:55:30 +0200

    version ok

--- OTROS/ahootsa_v0_3_6_ollama_chat_logging_fix/src/ahootsa_reachy_mini_desktop_app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Ahootsa local apps for Reachy Mini Desktop."""
-
-__version__ = "0.3.0"
--- OTROS/ritxi_fastapi_v5_9_95_activity_animation_audio_final_fix/2_INICIAR_DAEMON_RITXI.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-title 2 - DAEMON RITXI / REACHY MINI
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp02_INICIAR_DAEMON_RITXI.ps1"
